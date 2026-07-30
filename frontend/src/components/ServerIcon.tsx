9962d71024e292fffddf3a38ed445ae287ebead1
Ari <afazola@gmail.com>
2026-07-30 16:08:07 +0700

    image error fallback

--- frontend/src/components/ServerIcon.tsx
+++ frontend/src/components/ServerIcon.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 interface ServerIconProps {
   path: string
 }
 const ServerIcon = ({ path }: ServerIconProps) => {
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  const onImageError = () => {
+    if (imgRef.current === null) {
+      return
+    }
+    imgRef.current.src = "/logo.png"
+  }
   return (
     <div className='rounded-lg bg-primary w-full h-full flex items-center justify-center'>
-      <img src={`${import.meta.env.VITE_S3_BUCKET_URL}${path}`} className='w-full h-full rounded-lg' />
+      <img src={`${import.meta.env.VITE_S3_BUCKET_URL}${path}`} className='w-full h-full rounded-lg' ref={imgRef} onError={onImageError} />
     </div>
   )
 }
