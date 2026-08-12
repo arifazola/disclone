212bfc4800434c61a5849327cdbdf2213c783585
Ari <afazola@gmail.com>
2026-08-12 23:51:20 +0700

    update api url

--- frontend/src/consts/const.ts
+++ frontend/src/consts/const.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://192.168.1.3:8080"
-export const BASE_WS = "wss://192.168.1.3:8080"
+export const BASE_URL = "https://disclone-api.dygytalyzed.com"
+export const BASE_WS = "wss://disclone-api.dygytalyzed.com"
